f8e07270367af204044e598db15ba9428ea3c48e
epitinov <evgeniy.pitinov@gmail.com>
2019-07-11 18:56:13 +0300

    add project

---
+++ TextFileConversion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TextFileConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter the file path and name of the file to be processed.");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
